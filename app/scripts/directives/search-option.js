75e5d4bc5177181e647da12ff31920448ded3404
Danny Blue <dannybluedesign@gmail.com>
2014-06-07 15:41:28 -0400

    'comments'

--- app/scripts/directives/search-option.js
+++ app/scripts/directives/search-option.js
@@ -12,19 +12,39 @@ angular.module('blueUiApp')
         optionTitle: '='
       },
       link: function postLink(scope) {
+        /*
+         * Sets default filter to blank
+         */
         scope.search = '';
 
+        /*
+         * Sets the currently selected option to the option title.
+         */
         scope.currentOption = scope.optionTitle;
 
+        /*
+         * Prevents the menu from closing when clicking on the text box
+         */
         scope.preventClose = function($event) {
           $event.stopPropagation();
         };
 
+        /*
+         * Method for selecting an option.
+         */
         scope.selectOption = function($event, currentItem) {
           $event.preventDefault();
 
+          /*
+           * Sets the currently displayed option to the selected option
+           */
           scope.currentOption = currentItem;
 
+          /*
+           * If a custom click method is defined fire the method.
+           * Passes in angular.element($event.target) (the clicked item)
+           * as an argument
+           */
           if(scope.customClick !== undefined) {
             scope.customClick(angular.element($event.target));
           }
