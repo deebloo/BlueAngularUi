d9a385a5716fd5f36a52a553c5c14c18dd09a426
Danny Blue <dannybluedesign@gmail.com>
2014-06-07 20:54:33 -0400

    Fix url hashbang comparison

--- app/scripts/directives/navbar.js
+++ app/scripts/directives/navbar.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('blueUiApp')
+  /*
+   * REQUIRES $locationProvider TO BE SET UP IN APP CONFIG
+   */
   .directive('navbar', function ($location) {
     return {
       templateUrl: 'views/navbar.html',
@@ -9,11 +12,22 @@ angular.module('blueUiApp')
         data: '='
       },
       link: function postLink(scope) {
-        console.log($location.path());
-
-        // TODO work out hashbanging madness!
         scope.activeNav = function(link) {
-          return '#!' + $location.path() === link;
+          /*
+           * If the route contains '#!', remove it for comparison
+           * (HTML5 mode === false || HTML5 mode === true && older browser)
+           * if not leave it alone
+           * (HTML5 mode === true && in modern browser)
+           */
+          var route = function() {
+            if( link.indexOf('#!') > -1 ) {
+              return link.replace('#!', '');
+            } else {
+              return link;
+            }
+          }();
+
+          return $location.path() === route;
         }
       }
     };
